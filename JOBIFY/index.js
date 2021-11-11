6d781ae6cc5ed25214792016e1be019900b24851
Rodrigo Weber <rodrigoweber@MacBook-Pro-de-Rodrigo.local>
2021-11-11 07:20:34 -0300

    jobify_project

--- JOBIFY/index.js
+++ JOBIFY/index.js
@@ -2,13 +2,20 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 console.log(1)
 app.get('/', (request, response) => {
-	console.log(new Date)
 	//response.send('<h1>Olá Fullstack Lab</h1>')
-	response.render('home')
+	response.render('home', {
+	})
 })
+
+app.get('/vaga', (request, response) => {
+	response.render('vaga', {
+	})
+})
+
 console.log(2)
 app.listen(3000, err => {
 	if(err){
